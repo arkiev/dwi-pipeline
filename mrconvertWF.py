4582985f830bb1d16ccec984e0ed77f12123bdf9
adsouza <arkievdsouza@sydney.edu.au>
2023-07-24 12:38:13 +1000

    debugged basic workflow

--- mrconvertWF.py
+++ mrconvertWF.py
@@ -1,36 +1,56 @@
 import os
+import typing as ty
 import pydra  
 from pydra import Workflow, mark
 from pydra.engine.specs import File
 from pydra.tasks.mrtrix3.latest import mrconvert
 
-from fileformats.medimage import NiftiGzXBvec, NiftiGzX
+from fileformats.medimage import NiftiGzXBvec, NiftiGz, MrtrixImage
 
 # Define the path and output_path variables
 path = '/Users/arkievdsouza/Documents/NIFdata/ds000114'
 output_path = '/Users/arkievdsouza/git/dwi-pipeline/working-dir'
 
 # Define the input_spec for the workflow
-input_spec = {"dwi": NiftiGzXBvec, "t1w": NiftiGzX, "bvec": File, "bval": File}
-output_spec = {"dwi_preproc": NiftiGzXBvec}
+input_spec = {"dwi": NiftiGz, "t1w": NiftiGz, "bvec": File, "bval": File}
+output_spec = {"dwi_preproc": MrtrixImage}
 
 # Create a workflow and add the dg task
 wf = Workflow(name='my_workflow', input_spec=input_spec) 
+
+@mark.task
+def merge_grads(bvec: File, bval: File) -> ty.Tuple[File, File]:
+    return (bvec, bval)
+
+
+wf.add(
+    merge_grads(
+        bvec=wf.lzin.bvec,
+        bval=wf.lzin.bval,
+        name="merge_grads",
+    )
+)
 
 #mrconvert
 wf.add(
     mrconvert.mrconvert(
         input=wf.lzin.dwi,
-        fslgrad=[wf.lzin.bvec, wf.lzin.bval],
+        fslgrad=wf.merge_grads.lzout.out,
+        output="converted.mif",
         name="dwi_mif"
     )
 )
+
+wf.set_output(("dwi_preproc", wf.dwi_mif.lzout.output))
 
 
 # Execute the workflow
 result = wf(
     dwi="/Users/arkievdsouza/Documents/NIFdata/ds000114/sub-01/ses-retest/dwi/sub-01_ses-retest_dwi.nii.gz",
-    t1w="/Users/arkievdsouza/Documents/NIFdata/ds000114/sub-01/ses-retest/anat/sub-01_ses-01_t1w.nii.gz",
+    t1w="/Users/arkievdsouza/Documents/NIFdata/ds000114/sub-01/ses-retest/anat/sub-01_ses-retest_T1w.nii.gz",
     bvec="/Users/arkievdsouza/Documents/NIFdata/ds000114/dwi.bvec",
-    bval="/Users/arkievdsouza/Documents/NIFdata/ds000114/dwi.bval"
+    bval="/Users/arkievdsouza/Documents/NIFdata/ds000114/dwi.bval",
+    plugin="serial",
 )
+
+print(f"Processed output generated at '{result.output.dwi_preproc}'")
